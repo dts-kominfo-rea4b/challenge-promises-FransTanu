75325df1f29f19b485e348147024fc724ee46259
FransTanu <Franstan1112@gmail.com>
2022-09-17 02:33:30 +0700

    commit tugas

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasilPenonton) => {
+  let hasil = 0;
+  const hasilTheaterIXX = await promiseTheaterIXX(hasilPenonton);
+  const hasilTheaterVGC = await promiseTheaterVGC(hasilPenonton);
+
+  // Perulangan
+  for (const element of hasilTheaterIXX) {
+    if (hasilPenonton === element.hasil) {
+      hasil++;
+    }
+  }
+  for (const element of hasilTheaterVGC) {
+    if (hasilPenonton === element.hasil) {
+      hasil++;
+    }
+  }
+  return hasil
+};
 
 module.exports = {
   promiseOutput,
